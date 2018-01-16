f484dff05390986aea03f233b97082c316182f46
EMIL-DATOR\Emil <emil.emanuel@hotmail.com>
2018-01-16 13:44:43 +0100

    SQL add queries, and all tables

--- src/Model/Book.java
+++ src/Model/Book.java
@@ -3,24 +3,22 @@ package Model;
 public class Book {
 	private int id;
 	private String title;
-	private String author;
 	private boolean loaned;
 	private String description;
-	private String category;
 	private String edition;
 	private String published;
-	private String subject;
+	private int shelfNo;
 
-	public Book (int id, String title, String author, String description, String category, String edition, String published, String subject) {
+	public Book (int id, String title,  String description, String edition, String published,  int shelf) {
 		this.id = id;
 		this.title = title;
-		this.author = author;
 		this.description = description;
-		this.category = category;
 		this.edition = edition;
 		this.published = published;
-		this.subject = subject;
-		this.loaned = false;
+		this.shelfNo = shelf;
+	}
+	public int getShelfNo() {
+		return this.shelfNo;
 	}
 	public int getId() {
 		return this.id;
@@ -28,23 +26,15 @@ public class Book {
 	public String getDescription() {
 		return this.description;
 	}
-	public String getCategory() {
-		return this.category;
-	}
 	public String getPublished() {
 		return this.published;
 	}
-	public String getText() {
-		return this.subject;
-	}
+
 	public void setLoaned(boolean b) {
 		this.loaned = b;
 	}
 	public String getTitle() {
 		return this.title;
-	}
-	public String getAuthor() {
-		return this.author;
 	}
 	public boolean getLoaned() {
 		return this.loaned;
