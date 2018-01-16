5c7d6704633051c211ae426195e20e13a9d5314f
Emil <emil.emanuel@hotmail.com>
2018-01-16 15:00:05 +0100

    added some methods to library

--- src/Model/Library.java
+++ src/Model/Library.java
@@ -4,10 +4,16 @@ public class Library {
 	private Book book;
 	private Person person;
 
-	public void addBook() {
+	public void addBook(int id,String title,  String description, String edition, String published,  int shelf) {
+		this.book = new Book(id, title, description, edition, published, shelf);
+	}
+	public void addPerson(int id, String zIP, String city, String adress, String name, String mail, String phoneNr) {
+		this.person = new Person(id, zIP, city, adress, name, mail, phoneNr);
+	}
+	public void addLoan() {
 		
 	}
-	public void addPerson() {
+	public void addCategory() {
 		
 	}
 }
