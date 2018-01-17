52ffd34a7e1cc4fdebc9b56d03a0c8de1bcf607a
Emil <emil.emanuel@hotmail.com>
2018-01-17 14:07:51 +0100

    working on input in controller and added a program class for starting and looping the library system

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -1,6 +1,47 @@
 package controller;
 
+import View.View;
 
 public class controller {
+	private View a_view;
 	
+	public boolean runLibrarySystem(View a_view)
+	{
+		this.a_view = a_view;
+		a_view.DisplayWelcomeMessage();
+		
+		char[] menuOptions = a_view.getMenuOptions();
+		int input = a_view.getInput();
+		
+		if (input == menuOptions[0])
+		{
+			
+		}
+		else if (input == menuOptions[1])
+		{
+			
+		}
+		else if (input == menuOptions[2])
+		{
+			
+		}
+		else if (input == menuOptions[3])
+		{
+			
+		}
+		else if (input == menuOptions[4])
+		{
+			
+		}
+		else if (input == menuOptions[5])
+		{
+			
+		}
+		else if (input == menuOptions[6])
+		{
+			
+		}
+		
+		return input != menuOptions[7];
+	}
 }
